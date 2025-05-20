8c562dfb9083dd0e6df5f8a97e42d5531423de37
Aaron Ostroski <aaronmostroski@gmail.com>
2025-05-20 19:05:23 -0300

    feat: implementar filtragem de pastas e arquivos na página inicial

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,7 +1,22 @@
+require 'ostruct'
+
 class Site::HomeController < SiteController
   def index
-    @folders = Folder.order(:name)
-    @archives = Archive.order(:name)
+    @filters = parse_filters
+        
+    folders = Folder.all
+    folders = folders.where(parent_folder_id: @filters.folder_id) if @filters.folder_id
+    @folders = folders.order(:name)
+
+    archives = Archive.all
+    archives = archives.where(folder_id: @filters.folder_id) if @filters.folder_id
+    @archives = archives.order(:name)
+  end
+
+  private
+
+  def parse_filters
+    @filters = OpenStruct.new(params[:filters])
   end
 end
   
