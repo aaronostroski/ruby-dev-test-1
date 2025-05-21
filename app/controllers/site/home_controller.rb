13b050192cec4ad2553d2c191e88d9d00459434d
Aaron Ostroski <aaronmostroski@gmail.com>
2025-05-21 02:24:33 -0300

    feat: adicionar paginação para arquivos e pastas na página inicial

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,26 +1,27 @@
 require 'ostruct'
 
 class Site::HomeController < SiteController
-  before_action :set_filters, :set_archives, :set_folders, :set_current_folder, :set_totals, only: [:index]
+  PER_PAGE = 11
+  before_action :set_filters, :set_folders, :set_archives, :set_current_folder, :set_totals, only: [:index]
 
   def index; end;
 
   private
-
-  def set_archives
-    archives = Archive.all    
-    archives = archives.where(folder_id: @filters.folder_id) if @filters.folder_id.present?
-    archives = archives.where(folder_id: nil) unless @filters.folder_id&.present?
-
-    @archives = archives.order(:name)
-  end
 
   def set_folders
     folders = Folder.all
     folders = folders.where(parent_folder_id: @filters.folder_id) if @filters.folder_id.present?
     folders = folders.where(parent_folder_id: nil) unless @filters.folder_id&.present?
 
-    @folders = folders.order(:name)
+    @folders = folders.order(:name).page(params[:folders_page]).per(PER_PAGE)
+  end
+
+  def set_archives
+    archives = Archive.all    
+    archives = archives.where(folder_id: @filters.folder_id) if @filters.folder_id.present?
+    archives = archives.where(folder_id: nil) unless @filters.folder_id&.present?
+
+    @archives = archives.order(:name).page(params[:archives_page]).per(PER_PAGE)
   end
 
   def set_current_folder
